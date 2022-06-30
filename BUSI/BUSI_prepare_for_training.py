c29b13de9f5820868f6bb362be571157ed3e71a3
yyn10152333 <70582598+yyn10152333@users.noreply.github.com>
2022-06-30 23:39:38 +0800

    Update BUSI_prepare_for_training.py

--- BUSI/BUSI_prepare_for_training.py
+++ BUSI/BUSI_prepare_for_training.py
@@ -5,7 +5,7 @@ import os
 # os.rename('BUSI/BUSI_train_complete', 'BUSI/BUSI_train')
 
 gdd.download_file_from_google_drive(file_id='Dataset_BUSI.zip',
-                                    dest_path='./BUSI/BUSI.zip',
+                                    dest_path='./BUSI/Dataset_BUSI.zip',
                                     unzip=True)
 
-os.rename('BUSI/BUSI', 'BUSI/BUSI_train')
+os.rename('BUSI/Dataset_BUSI_with_GT', 'BUSI/BUSI_train')
